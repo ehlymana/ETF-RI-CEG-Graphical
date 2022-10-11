4d04330a39d52923a0856e0a71cbb3fb203236bb
Ehlimana <ekrupalija1@etf.unsa.ba>
2022-10-11 21:36:13 +0200

    ETF-RI-CEG tool with build and release folders

---
+++ CauseEffectGraphGraphical/CauseEffectGraph/Node.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CauseEffectGraph
+{
+    public class Node
+    {
+        #region Properties
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Index { get; set; }
+        public string Type { get; set; }
+        public bool Result { get; set; }
+
+        #endregion
+
+        #region Constructor
+
+        public Node (int x, int y, int index, string type)
+        {
+            X = x;
+            Y = y;
+            Index = index;
+            Type = type;
+        }
+
+        #endregion
+    }
+}
